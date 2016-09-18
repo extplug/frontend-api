0b7276dd54ed5039a9991765a36069bc991ebee8
René Kooi <renee@kooi.me>
2016-09-18 16:29:10 +0200

    use lodash-es

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
-import { join } from 'path'
+import * as path from 'path'
 import { readFileSync } from 'fs'
 import babel from 'rollup-plugin-babel'
+import alias from 'rollup-plugin-alias'
 
-const meta = JSON.parse(readFileSync(join(__dirname, './package.json'), 'utf8'))
+const meta = JSON.parse(readFileSync(path.join(__dirname, './package.json'), 'utf8'))
 
 export default {
   entry: './src/index.js',
@@ -16,7 +17,11 @@ export default {
       ],
       plugins: [
         'external-helpers'
-      ]
+      ],
+      include: 'src/**.js'
+    }),
+    alias({
+      lodash: path.dirname(require.resolve('lodash-es'))
     })
   ]
 }
